c10be4e19e797321dc9687c08f35bbe94cb6c4af
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-09-27 10:40:43 -0400

    Add files via upload

--- client/client.cpp
+++ client/client.cpp
@@ -1,1 +1,22 @@
-// runs on client machine
+#include "client.h"
+//#include <sys/socket.h> unix
+#include <winsock2.h> // windows
+
+#pragma comment(lib, "Ws2_32.lib") // ignored by MinGW but harmless
+
+int main()
+{
+	SOCKET sock = INVALID_SOCKET;
+	// SOCK_RAW would provide more control but is more complicated and requires elevated perms
+	// IPPROTO_TCP can be NULL instead? if NULL, kernel chooses protocal... tcp by default
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
+
+	SOCKADDR_IN addr;
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)))
+	{
+		// throw error
+		return 1;
+	}
+
+	return 0;
+}
---
+++ client/client.h
@@ -0,0 +1,4 @@
+
+const char* server_ip = "10.0.0.86";
+const int port = 4444;
+//const char* port = "4444";
