607afb5d1b5cafeb88c2f4330f0aeddc671317a6
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-09-30 13:13:51 -0400

    Update AntiDebug.h

--- modules/evasion/AntiDebug.h
+++ modules/evasion/AntiDebug.h
@@ -3,5 +3,15 @@ class AntiDebug
 public:
     AntiDebug();
     ~AntiDebug();
+
+    bool debugFlags();
+    bool objectHandles();
+    bool exceptions();
+    bool timing();
+    bool processMemory();
+    bool assemblyInstructions();
+    bool directDebug();
+    bool misc();
+
 private:
 };
