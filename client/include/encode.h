334e5edb961e37f6fe3c171c6d55c2660408839f
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-10-15 10:33:24 -0400

    Update encode.h

--- client/include/encode.h
+++ client/include/encode.h
@@ -2,3 +2,6 @@
 /* encode json into base64 */
 /* encoding is needed for http, smtp, json, xml, html, urls,  (but not tcp) */
 /* tcp/udp, smb, dns, ftp, and other binary traffic protocols don't need encoding */
+
+
+/* define differnt encoding formats in here... start with TLV */
