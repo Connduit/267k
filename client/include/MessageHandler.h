9337237238894d08a6fb06b1ed7a442c14e96560
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-10-22 08:16:51 -0400

    Update MessageHandler.h

--- client/include/MessageHandler.h
+++ client/include/MessageHandler.h
@@ -1,8 +1,36 @@
 /* MessageHandler.h */
 
+#include "Compressor.h"
+#include "C2Profile.h"
+#include "Encoder.h"
+#include "Encrypter.h"
+#include "Serializer.h"
+
+#include <memory>
+
+typedef std::unique_ptr<Compressor> CompressorUniquePtr;
+typedef std::unique_ptr<Encoder> EncoderUniquePtr;
+typedef std::unique_ptr<Encrypter> EncrypterUniquePtr;
+typedef std::unique_ptr<Serializer> SerializerUniquePtr;
+
 class MessageHandler
 {
 public:
+    MessageHandler(
+            CompressorUniquePtr compressorPtr,
+            EncrypterUniquePtr encrypterPtr,
+            EncoderUniquePtr encoderPtr,
+            SerializerUniquePtr serializerPtr,
+            Config* config);
+    
+    bool sendMessage(); // overload this function?
+    bool recvMessage(); // overload this function?
+            
 private:
-}
+    CompressorUniquePtr compressorPtr_;
+    EncrypterUniquePtr encrypterPtr_;
+    EncoderUniquePtr encoderPtr_;
+    SerializerUniquePtr serializerPtr_;
+    Config config_;
+};
 
