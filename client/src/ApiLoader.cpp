70bf058a09882bb341503498f066e9bdd12bafd9
Connor Finch <49081590+Connduit@users.noreply.github.com>
2025-10-28 08:25:31 -0400

    Implement manual module and procedure handling methods

--- client/src/ApiLoader.cpp
+++ client/src/ApiLoader.cpp
@@ -1,2 +1,23 @@
 
 #include "ApiLoader.h"
+
+
+HMODULE ApiLoader::GetModuleHandleManual(LPCWSTR lpModuleName)
+{
+    
+}
+
+HMODULE ApiLoader::GetModuleHandleManual(DWORD moduleHash)
+{
+    
+}
+
+FARPROC ApiLoader::GetProcAddressManual(PVOID pModule, LPCSTR lpProcName)
+{
+    
+}
+
+FARPROC ApiLoader::GetProcAddressManual(HMODULE hModule, DWORD procHash)
+{
+    
+}
